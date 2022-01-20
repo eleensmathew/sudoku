a429fdb48136575f4da5ea8a59dc75d69f177eaa
Eleena <eleensmathew@gmail.com>
2022-01-20 18:25:13 +0300

    completed file

--- sudoku.py
+++ sudoku.py
@@ -1,25 +1,17 @@
-board= [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7],
-     
-]
+board=[]
+for i in range(9):
+    board=board+[eval(input('Enter list of 9 numbers\n Enter 0 for blanks: '))]
+
 
 def check(boar,nu,pos):
     #check if no is there in the row
     for i in range(len(boar)):
         if boar[pos[0]][i]==nu and pos [1]!=i:
-            return false
+            return False
     #check if no is there in coloumn
     for i in range(len(boar)):
         if boar[i][pos[1]]==nu and pos[0]!=i:   
-            return false
+            return False
     #chcek in the box if no is there
     start_r=pos[0]-pos[0]%3
     start_c=pos[1]-pos[1]%3
@@ -27,7 +19,7 @@ def check(boar,nu,pos):
     for  i in range (start_r,start_r+3):
         for j in range (start_c,start_c+3):
                 if boar[i][j]==nu and i!=pos[0] and j != pos[1]:
-                    return false
+                    return False
     return True                  
 
 def print_board(boar):    #prints the board
@@ -41,13 +33,31 @@ def print_board(boar):    #prints the board
                 print(boar[i][j])
             else:
                 print(str(boar[i][j])+' ',end='')
-print_board(board)                
 
 def find_zero(boar):
-    for in in range(len(boar)):
-        for j in length(len(boar)):
+    for i in range(len(boar)):
+        for j in range(len(boar)):
             if boar[i][j]==0:
                 return i,j   #returns row and column
+    return None
+#checks if we solved the puzzle
+def solve(boar):
+    find = find_zero(boar)
+    if not find:
+        return True
+    else:
+        row,col=find
+    for i in range (1,10):
+            if check(boar,i,(row,col)):
+                boar[row][col]=i
+                if solve(boar):
+                    return True
+                boar[row][col]=0
+    return False 
 
-
-
+print_board(board)
+print()
+print()
+solve(board)
+print_board(board)
+     
